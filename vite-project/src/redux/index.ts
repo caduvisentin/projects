fad93a0c022c6daadbb2ac4c4055da72b637ee54
caduvisentin <eduardovisentin@gmail.com>
2023-10-23 15:25:45 +0200

    estado global com redux

---
+++ vite-project/src/redux/index.ts
@@ -0,0 +1,24 @@
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+type ActionType = {
+    type: string,
+}
+
+const INITIAL_STATE = {
+    count: 0,
+}
+
+const reducer = (state = INITIAL_STATE, action: ActionType) => {
+    switch (action.type) {
+        case "INCREMENT_COUNTER":
+            return {
+                count: state.count+1,
+            }
+        default: return state;
+    }
+};
+
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
